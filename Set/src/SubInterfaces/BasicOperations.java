6bb8716bbe26e02d549a238cab5f9e24a4b5aa56
GangisettyRuchitha <ruchigangisetty@gmail.com>
2021-07-03 17:12:02 +0530

    Added Set Method Programs

---
+++ Set/src/SubInterfaces/BasicOperations.java
@@ -0,0 +1,24 @@
+package SubInterfaces;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+public class BasicOperations {
+
+	public static void main(String[] args) 
+	{
+		LinkedHashSet<Integer> h=new LinkedHashSet<Integer>();
+		h.add(10);
+		h.add(20);
+		h.add(30);
+		h.add(40);
+		h.add(10);
+		
+		System.out.println(h);
+		System.out.println(h.isEmpty());
+		System.out.println(h.size());
+		
+
+	}
+
+}
---
+++ Set/src/SubInterfaces/EnumerationDemo.java
@@ -0,0 +1,25 @@
+package SubInterfaces;
+
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class EnumerationDemo {
+
+	public static void main(String[] args) 
+	{
+		Vector<Integer> v=new Vector<Integer>();
+		for(int i=0;i<10;i++)
+		{
+			v.add(i);
+		}
+		System.out.println(v);
+		
+		Enumeration<Integer> e=v.elements();
+		while(e.hasMoreElements())
+		{
+			System.out.println(e.nextElement());
+		}
+
+	}
+
+}
