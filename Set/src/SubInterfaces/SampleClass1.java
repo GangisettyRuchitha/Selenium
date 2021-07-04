34fd67b693798d97220150263f57142831df60e8
GangisettyRuchitha <ruchigangisetty@gmail.com>
2021-07-04 16:28:47 +0530

    added a new message in the Sample Class1

--- Set/src/SubInterfaces/SampleClass1.java
+++ Set/src/SubInterfaces/SampleClass1.java
@@ -9,6 +9,7 @@ public class SampleClass1 {
 		System.out.println("dirty mann");
 		System.out.println("you don't have any manners");
 		System.out.println("la lala lala lallaaa");
+		System.out.println("ruchi papa");
 
 	}
 
