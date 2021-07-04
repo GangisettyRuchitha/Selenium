65fb125ce7101482c44fabc4022e4d5adccaf83b
GangisettyRuchitha <ruchigangisetty@gmail.com>
2021-07-04 16:24:23 +0530

    added a new sample class

---
+++ Set/src/SubInterfaces/SampleClass.java
@@ -0,0 +1,13 @@
+package SubInterfaces;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hey mann");
+		System.out.println("dirty mann");
+		System.out.println("you don't have any manners");
+		System.out.println("la lala lala lallaaa");
+
+	}
+
+}
