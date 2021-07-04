1377148359075981fd0476cb87f29b9ed9776c33
GangisettyRuchitha <ruchigangisetty@gmail.com>
2021-07-04 16:26:08 +0530

    Added a Sample class1 in the project

---
+++ Set/src/SubInterfaces/SampleClass1.java
@@ -0,0 +1,15 @@
+package SubInterfaces;
+
+public class SampleClass1 {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hey mann");
+		System.out.println("dirty mann");
+		System.out.println("you don't have any manners");
+		System.out.println("la lala lala lallaaa");
+
+	}
+
+}
